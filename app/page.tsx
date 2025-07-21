463f33a95701363f1888e5a4c80f912e76d41f41
mizuki <mizuki@Yota-KnoMacBook-Pro.local>
2025-07-21 22:26:59 +0900

    TOPページ一部実装

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,131 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { Barlow_Condensed } from "next/font/google";
+
+const barlowCondensed = Barlow_Condensed({
+  subsets: ["latin"],
+  weight: ["400", "600", "700"], // 必要なウェイトを指定
+  display: "swap",
+});
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <main>
+      <section className={styles.fv}>
+        <div className={`inner ${styles.fv_content}}`}>
+          <h1 className={`${styles.fv_title} ${barlowCondensed.className}`}>
+            Visionary
+            <br />
+            Mate
+          </h1>
+          <p className={styles.fv_subtitle}>
+            WEB関連で気になったトピックをシェアしています。
+          </p>
+          <div className={styles.fv_lead}>
+            <p>開発者体験・</p>
+            <p>UX体験の追求</p>
+          </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </section>
+
+      <section>
+        <div>
+          <h2>News</h2>
+          <ul>
+            <li>
+              <Image></Image>
+              <h4>タイトル</h4>
+              <p>タイトル</p>
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <section className={`inner ${styles.concept}`}>
+        <div className={styles.concept_content}>
+          <div className={styles.concept_content_left}>
+            <h2 className={`section_title ${barlowCondensed.className}`}>
+              Concept
+            </h2>
+            <h3>方針</h3>
+            <p>
+              デザイナー兼エンジニアとして、情報収集の一環として備忘録的に記録しています。
+            </p>
+          </div>
+
+          <div className={styles.concept_content_right}>
+            <Image src="/image/design.png" alt="" width={240} height={240} />
+          </div>
+        </div>
+      </section>
+
+      <section className={`inner ${styles.aboutme}`}>
+        <div className={styles.aboutme_content}>
+          <div className={styles.aboutme_content_left}>
+            <Image
+              src="/image/photo of me.png"
+              alt=""
+              width={310}
+              height={350}
+            />
+          </div>
+          <div className={styles.aboutme_content_right}>
+            <h2 className={`section_title ${barlowCondensed.className}`}>
+              About me
+            </h2>
+            <h3>私について</h3>
+            <p>
+              デザイナー兼エンジニアとして、情報収集の一環として備忘録的に記録しています。
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <section className={`inner ${styles.Service}`}>
+        <h2 className={`section_title ${barlowCondensed.className}`}>
+          Service
+        </h2>
+        <h3>事業内容</h3>
+
+        <ul className={styles.service_list}>
+          <li>
+            <Image
+              src="/image/HPLP.png"
+              alt="HP/LP制作"
+              width={300}
+              height={200}
+            />
+            <p>HP/LP制作</p>
+          </li>
+          <li>
+            <Image
+              src="/image/Banner.png"
+              alt="バナー制作"
+              width={300}
+              height={200}
+            />
+            <p>バナー制作</p>
+          </li>
+          <li>
+            <Image
+              src="/image/UIUX.png"
+              alt="UIUX改善"
+              width={300}
+              height={200}
+            />
+            <p>UIUX改善</p>
+          </li>
+          <li>
+            <Image
+              src="/image/Maintenance.png"
+              alt="保守運用管理"
+              width={300}
+              height={200}
+            />
+            <p>保守運用管理</p>
+          </li>
+        </ul>
+      </section>
+    </main>
   );
 }
