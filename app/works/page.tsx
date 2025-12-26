20fa3b5e39613a9fd0d33d22aed53c45d80ce921
mizuki <mizuki@Yota-KnoMacBook-Pro.local>
2025-12-26 16:18:03 +0900

    feat: 制作実績のAPI実装

--- app/works/page.tsx
+++ app/works/page.tsx
@@ -2,6 +2,9 @@ import styles from "./page.module.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { Barlow_Condensed } from "next/font/google";
+import Link from "next/link";
+import Image from "next/image";
+import { getWorksList } from "../libs/microcms";
 
 const barlowCondensed = Barlow_Condensed({
   subsets: ["latin"],
@@ -9,7 +12,15 @@ const barlowCondensed = Barlow_Condensed({
   display: "swap",
 });
 
-export default function page() {
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
+
+  const workList = await getWorksList(slug);
+
   return (
     <>
       <Header />
@@ -18,9 +29,45 @@ export default function page() {
           <h2 className={`${styles.works_title} ${barlowCondensed.className}`}>
             Works
           </h2>
+
+          <ul className={styles.news_list}>
+            {workList.contents.map((works) => (
+              <li key={works.id} className={styles.works_item}>
+                <Link href={`/works/${works.id}`}>
+                  <div className={styles["works__item-images"]}>
+                    <div className={styles.works_image}>
+                      {works.workImage ? (
+                        <Image
+                          src={works.workImage.url}
+                          alt={works.title}
+                          className={styles.works_image}
+                          width={300}
+                          height={200}
+                          style={{ objectFit: "contain" }}
+                        />
+                      ) : (
+                        <Image
+                          src="/image/noimage.jpg"
+                          alt="No Image"
+                          className={styles.works_image}
+                          width={300}
+                          height={200}
+                          style={{ objectFit: "contain" }}
+                        />
+                      )}
+                    </div>
+                  </div>
+                  <div className={styles["works__item-detail"]}>
+                    <h3>{works.title}</h3>
+                    <h4>{works.subtitle}</h4>
+                    <p>{works.summary}</p>
+                  </div>
+                </Link>
+              </li>
+            ))}
+          </ul>
         </div>
       </section>
-      <p className={styles.text}>Coming soon...</p>
       <Footer />
     </>
   );
